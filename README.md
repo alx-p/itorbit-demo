f1d3594a8e13f47b2b538b338e0f9587376221d7
alx-p <alex@itwh.ru>
2026-03-25 13:52:14 +0300

    v.1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # itorbit-demo
+
+Демонстрационные базы данных для проекта itOrbit::DataComparer (https://www.itorbit.ru/datacomparer/).
+
+Используемые СУБД: PostgreSQL и MySQL.
+
+Запускайте 
+```
+sudo docker compose up --build
+```
+и подключайтесь.
---
+++ postgres/init.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS table1 (
+    aircraft_code char(3) NOT NULL PRIMARY KEY,
+    model_en varchar(32) DEFAULT NULL,
+    model_ru varchar(32) DEFAULT NULL,
+    fly_distance int DEFAULT NULL);
+
+INSERT INTO table1 (aircraft_code,model_en,model_ru,fly_distance) VALUES
+	 ('319','Airbus A319-100','Аэробус A319-100',6700),
+	 ('320','Airbus A320-200','Аэробус A320-200',5700),
+	 ('321','Airbus A321-200','Аэробус A321-200',5600),
+	 ('733','Boeing 737-300','Боинг 737-300',4200),
+	 ('763','Boeing 767-300','Боинг 767-300',7900),
+	 ('773','Boeing 777-300','Боинг 777-300',11100),
+	 ('CN1','Cessna 208 Caravan','Сессна 208 Караван',1200),
+	 ('CR2','Bombardier CRJ-200','Бомбардье CRJ-200',2700),
+	 ('SU9','Sukhoi Superjet-100','Сухой Суперджет-100',3000);
+
+CREATE TABLE IF NOT EXISTS table2 (
+    aircraft_code char(3) NOT NULL PRIMARY KEY,
+    model_en varchar(32) DEFAULT NULL,
+    model_ru varchar(32) DEFAULT NULL,
+    fly_distance int DEFAULT NULL);
+
+INSERT INTO table2 (aircraft_code,model_en,model_ru,fly_distance) VALUES
+	 ('319','Airbus A319-100','Аэробус A319-100',6700),
+	 ('320','Airbus A320-200','Аэробус A320-200',5700),
+	 ('321','Airbus A321-200',NULL,5600),
+	 ('733','Boeing 737-300','Боинг 737-300',4200),
+	 ('763',NULL,'Боинг 767-300',7900),
+	 ('773','Boeing 777-300','Боинг 777-300',11100),
+	 ('CN1','Cessna 208 Caravan','Сессна 208 Караван',NULL),
+	 ('CR2','Bombardier CRJ-200','Бомбардье CRJ-200',2700),
+	 ('SU9','Sukhoi Superjet-100','Сухой Суперджет-100',3000);
